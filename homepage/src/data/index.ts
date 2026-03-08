fc8d6314d8073ece8562b004890bd3cc69bf7dab
Yansu <no-reply@yansu.ai>
2026-03-08 04:36:35 +0000

    test(data): add content maintainability tests (NFR-5)
    Verify that content can be easily updated without build tools:
    - Test for features data file separation
    - Test for getting started steps in data file
    - Test for roadmap items in data file
    - Test for links/URL constants editability
    - Update index.ts to export all data modules

--- homepage/src/data/index.ts
+++ homepage/src/data/index.ts
@@ -1,6 +1,11 @@
 /**
  * Data exports for MirDB Homepage
+ *
+ * This file aggregates all content data files for easy importing.
+ * Content is separated from components to enable easy updates (NFR-5).
  */
 
 export * from './features'
+export * from './gettingStarted'
+export * from './roadmap'
 export * from './links'
