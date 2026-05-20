5ab651ba83746198621a199c0a506c660d7f4f0c
Yansu <no-reply@yansu.ai>
2026-05-20 03:33:31 +0000

    chore: remove node_modules and add to gitignore

--- node_modules/playwright-core/lib/entry/cliDaemon.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var import_coreBundle = require("../coreBundle");
-const { program } = require("../utilsBundle");
-import_coreBundle.tools.decorateCliDaemonProgram(program);
-void program.parseAsync();
--- node_modules/playwright-core/lib/entry/dashboardApp.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-var import_coreBundle = require("../coreBundle");
-import_coreBundle.tools.openDashboardApp();
